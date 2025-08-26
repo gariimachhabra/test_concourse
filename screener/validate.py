2a0df69eac93955a8595dc16d703639b8bf085ef
Garima <garima.chhabra@godigitaltc.com>
2025-08-26 11:52:17 +0530

    updated

--- screener/validate.py
+++ screener/validate.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-engine = create_engine(f'postgresql+psycopg2://concourse_user:concourse_pass@127.24.0.2:5432/concourse')
+engine = create_engine(f'postgresql+psycopg2://concourse_user:concourse_pass@172.24.0.2:5432/concourse')
 select_query = "SELECT * FROM yfinance_data;"
 df = pd.read_sql(select_query, engine)
 
