60a738ab72712a0205212edd91aaf96179bf08ef
Garima <garima.chhabra@godigitaltc.com>
2025-08-26 10:34:19 +0530

    table validate code

---
+++ screener/validate.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine(f'postgresql+psycopg2://concourse_user:concourse_pass@localhost:5432/concourse')
+select_query = "SELECT * FROM yfinance_data;"
+df = pd.read_sql(select_query, engine)
+
+print(df.head())
+print(df.info())
+print(df.describe())
